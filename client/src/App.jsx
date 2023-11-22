da4655deaedf4a49da16aa46964adac5efd1c15a
khalid586 <khalidcomilla58@gmail.com>
2023-11-22 16:52:00 +0600

    tailwind setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,13 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='bg-red-500'>
+        this is khalid
       </div>
       <h1>Vite + React</h1>
       <div className="card">
