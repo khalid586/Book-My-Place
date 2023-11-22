f6cbad625416e82868b1db6e12e340f747d12a1c
khalid <khalidcomilla58@gmail.com>
2023-11-22 18:37:52 +0600

    navbar designed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#ff385c'
+      }
+    },
   },
   plugins: [],
 }
