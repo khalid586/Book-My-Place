cc21671a69d86975d7f61436fa2b9c4f96991e2c
khalid586 <khalidcomilla58@gmail.com>
2023-11-30 01:58:00 +0600

    mongoose installed and connected to backend

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const { default: mongoose } = require('mongoose')
 const app = express()
+require('dotenv').config()
 
 app.use(express.json()) // without using this data won't converted in json format and can't be shown as well
 
@@ -8,6 +10,8 @@ app.use(cors({
     credentials: true,
     origin:'http://localhost:5173'
 }))
+
+mongoose.connect(process.env.MONGO_URL)
 
 app.post('/register',(req,res) =>{
     const {name,email,password} = req.body
@@ -19,3 +23,5 @@ app.get('/test',(req,res) =>{
 });
 
 app.listen(4000)
+
+// 
