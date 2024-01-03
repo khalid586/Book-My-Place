90e70cdf2e910a215c923a350a13d8aba4fc7eff
khalid586 <khalidcomilla58@gmail.com>
2024-01-03 21:10:16 +0600

    try catch block and alert implemented

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,13 +7,19 @@ export default function RegisterPage(){
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
 
-    function registerUser(ev){
+    async function registerUser(ev){
         ev.preventDefault();
-        axios.post('/register',{
-            name,
-            email,
-            password,
-        })
+        try{
+            await axios.post('/register',{
+                name,
+                email,
+                password,
+            })
+            alert('Registration successful. Now you can login!')
+        }catch(e){
+            alert('Registration failed. Please try again later!')
+        }
+        
     }
 
     return (
