5eff8662d096df6b75ce60d8fd5959e9a594b6db
khalid586 <khalidcomilla58@gmail.com>
2024-01-17 22:46:26 +0600

    landing page enhance

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -12,73 +12,91 @@ export default function IndexPage(){
         <div className="room-container">
             <div className="room-grid">
                 <div className="room-card">
-                <img src="images/img1.jpg" alt="Room 101" className="room-image" />
-                <div className="room-details">
-                    <p className="room-title">Room 101</p>
-                    <div className="room-rating">
-                    <i className="fa-solid fa-star"></i>
+                    <img src="images/img1.jpg" alt="Room 101" className="room-image" />
+                    <div className="room-details">
+                        <p className="room-title">Room 101</p>
+                        <div className="room-rating">
+                        <i className="fa-solid fa-star"></i>
+                        </div>
+                        <p className="room-date gray-text">22-27 Jul</p>
+                        <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
                     </div>
-                    <p className="room-date gray-text">22-27 Jul</p>
-                    <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
-                </div>
+                    <button className="btn"> Book Now</button>
                 </div>
                 <div className="room-card">
-                <img src="images/img1.jpg" alt="Room 101" className="room-image" />
-                <div className="room-details">
-                    <p className="room-title">Room 101</p>
-                    <div className="room-rating">
-                    <i className="fa-solid fa-star"></i>
+                    <img src="images/img1.jpg" alt="Room 101" className="room-image" />
+                    <div className="room-details">
+                        <p className="room-title">Room 101</p>
+                        <div className="room-rating">
+                        <i className="fa-solid fa-star"></i>
+                        </div>
+                        <p className="room-date gray-text">22-27 Jul</p>
+                        <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
                     </div>
-                    <p className="room-date gray-text">22-27 Jul</p>
-                    <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
-                </div>
+                    <button className="btn"> Book Now</button>
                 </div>
                 <div className="room-card">
-                <img src="images/img1.jpg" alt="Room 101" className="room-image" />
-                <div className="room-details">
-                    <p className="room-title">Room 101</p>
-                    <div className="room-rating">
-                    <i className="fa-solid fa-star"></i>
+                    <img src="images/img1.jpg" alt="Room 101" className="room-image" />
+                    <div className="room-details">
+                        <p className="room-title">Room 101</p>
+                        <div className="room-rating">
+                        <i className="fa-solid fa-star"></i>
+                        </div>
+                        <p className="room-date gray-text">22-27 Jul</p>
+                        <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
                     </div>
-                    <p className="room-date gray-text">22-27 Jul</p>
-                    <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
-                </div>
+                    <button className="btn"> Book Now</button>
                 </div>
                 <div className="room-card">
-                <img src="images/img1.jpg" alt="Room 101" className="room-image" />
-                <div className="room-details">
-                    <p className="room-title">Room 101</p>
-                    <div className="room-rating">
-                    <i className="fa-solid fa-star"></i>
+                    <img src="images/img1.jpg" alt="Room 101" className="room-image" />
+                    <div className="room-details">
+                        <p className="room-title">Room 101</p>
+                        <div className="room-rating">
+                        <i className="fa-solid fa-star"></i>
+                        </div>
+                        <p className="room-date gray-text">22-27 Jul</p>
+                        <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
                     </div>
-                    <p className="room-date gray-text">22-27 Jul</p>
-                    <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
-                </div>
+                    <button className="btn"> Book Now</button>
                 </div>
                 <div className="room-card">
-                <img src="images/img1.jpg" alt="Room 101" className="room-image" />
-                <div className="room-details">
-                    <p className="room-title">Room 101</p>
-                    <div className="room-rating">
-                    <i className="fa-solid fa-star"></i>
+                    <img src="images/img1.jpg" alt="Room 101" className="room-image" />
+                    <div className="room-details">
+                        <p className="room-title">Room 101</p>
+                        <div className="room-rating">
+                        <i className="fa-solid fa-star"></i>
+                        </div>
+                        <p className="room-date gray-text">22-27 Jul</p>
+                        <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
                     </div>
-                    <p className="room-date gray-text">22-27 Jul</p>
-                    <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
-                </div>
+                    <button className="btn"> Book Now</button>
                 </div>
                 <div className="room-card">
-                <img src="images/img1.jpg" alt="Room 101" className="room-image" />
-                <div className="room-details">
-                    <p className="room-title">Room 101</p>
-                    <div className="room-rating">
-                    <i className="fa-solid fa-star"></i>
+                    <img src="images/img1.jpg" alt="Room 101" className="room-image" />
+                    <div className="room-details">
+                        <p className="room-title">Room 101</p>
+                        <div className="room-rating">
+                        <i className="fa-solid fa-star"></i>
+                        </div>
+                        <p className="room-date gray-text">22-27 Jul</p>
+                        <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
                     </div>
-                    <p className="room-date gray-text">22-27 Jul</p>
-                    <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
+                    <button className="btn"> Book Now</button>
                 </div>
+                <div className="room-card">
+                    <img src="images/img1.jpg" alt="Room 101" className="room-image" />
+                    <div className="room-details">
+                        <p className="room-title">Room 101</p>
+                        <div className="room-rating">
+                        <i className="fa-solid fa-star"></i>
+                        </div>
+                        <p className="room-date gray-text">22-27 Jul</p>
+                        <p className="room-price text-sm font-medium mt-2">₹150 per day</p>
+                    </div>
+                    <button className="btn"> Book Now</button>
                 </div>
 
-                {/* Repeat the above structure for other rooms */}
+                
             </div>
         </div>
         </>
